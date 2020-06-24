93a1e13331c410eb84cdde31455fb92966bee9e2
Marten de Bruijn <martenbruijn@gmail.com>
2020-06-24 22:25:44 +0200

    added manifest.json as link in html pages

--- server/public/sw.js
+++ server/public/sw.js
@@ -1,38 +1,43 @@
-const staticCacheName = 'site-static-v1';
-const dynamicCache = 'site-dynamic-v1';
-const assets = ['/dist/all.js', '/dist/front.js', '/dist/styles.js'];
+const CORE_CACHE = 1;
+const CACHE_NAME = `core-cache-v${CORE_CACHE}`;
 
-// install service worker
-self.addEventListener('install', (evt) => {
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      cache.addAll(assets);
+const urlsToCache = [
+  //   '/callback',
+  '/dist/all.js',
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache).then(() => self.skipWaiting);
     })
   );
 });
 
-// activate event
-// self.addEventListener('activate', evt => {
-//   evt.waitUntil(
-//     caches.keys().then(keys => {
-//       return Promise.all(keys
-//         .filter(key => key !== staticCacheName && key !== dynamicCache)
-//         .map(key => caches.delete(key))
-//         )
-//     })
-//   )
-// });
+self.addEventListener('activate', (event) => {
+  console.log('Servive worker activated!');
+  event.waitUntil(clients.claim());
+});
 
-// // fetch events
-// self.addEventListener('fetch', evt => {
-//   evt.respondWith(
-//     caches.match(evt.request).then(cacheRes => {
-//       return cacheRes || fetch(evt.request).then(fetchRes => {
-//         return caches.open(dynamicCache).then(cache => {
-//           cache.put(evt.request.url, fetchRes.clone());
-//           return fetchRes;
-//         })
-//       })
-//     }).catch(() => caches.match('/fallback'))
-//   );
-// });
+self.addEventListener('fetch', (event) => {
+  console.log('fetch event: ', event.request);
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        console.log('Found in cache!');
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then((fetchResponse) => fetchResponse);
+      //   .catch((err => {
+
+      //     const isHTMLPage = event.request.method == "GET" && event.request.headers.get("accept").includes("text/html")
+
+      //     if(isHTMLPage) return caches.match("/offline")
+
+      //   }))
+    })
+  );
+});
